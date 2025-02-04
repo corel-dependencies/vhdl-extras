381dc460cc6e9d467f6e95f14a45fc6ed34d31d9
Jason J Watson <jason.watson@desy.de>
2025-02-04 11:35:25 +0100

    Remove dependency on corel-modules

--- vhdl_extras/__init__.py
+++ vhdl_extras/__init__.py
@@ -2,7 +2,6 @@ import importlib.metadata
 from typing import Optional
 
 from tsfpga.module_list import ModuleList
-from corel_modules.module.sysutils import get_modules
 from . import extras, extras_2008
 
 __version__ = importlib.metadata.version(__package__ or __name__)
@@ -10,7 +9,7 @@ __version__ = importlib.metadata.version(__package__ or __name__)
 def get_hdl_modules(
     names_include: Optional[set[str]] = None, names_avoid: Optional[set[str]] = None
 ) -> "ModuleList":
-
+    from corel_modules.module.sysutils import get_modules
     return get_modules(
         pymodules = [extras, extras_2008],
         names_include=names_include,
